0c2d097d9ca190cf56a1c705e3c03182d5225e59
mmmcy <133378649+mmmcy@users.noreply.github.com>
2024-08-27 08:04:33 -0300

    Add files via upload

---
+++ dudinha/js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["crys", "naju","yas","ana carol"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
